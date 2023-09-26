000e7f278cf6691292cff62907a9978dce381999
Neville Bamshoot <youcanteat@hotmail.com>
2023-09-25 22:18:22 -0400

    added biocard css and flex container, added font for p tags

--- src/Components/PageElements/BioCard/CardInfoData.jsx
+++ src/Components/PageElements/BioCard/CardInfoData.jsx
@@ -7,7 +7,9 @@ export const CardInfoData = [
     {
         id: 2,
         image: "BusinessGuy1",
-        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis quibusdam eaque ut minima labore! Recusandae atque obcaecati, dignissimos ullam molestiae voluptate, optio velit aperiam a, doloremque et. Doloribus, maxime suscipit!"
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis quibusdam eaque ut minima labore! Recusandae atque obcaecati, dignissimos ullam molestiae voluptate, optio velit aperiam a, doloremque et. Doloribus, maxime suscipit!" +
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis quibusdam eaque ut minima labore! Recusandae atque obcaecati, dignissimos ullam molestiae voluptate, optio velit aperiam a, doloremque et. Doloribus, maxime suscipit!" +
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem alias, voluptate fuga numquam illum consequatur aspernatur eveniet cum nemo eligendi molestias tempore veritatis, ipsa necessitatibus quasi quos recusandae non at."
     },
     {
         id: 3,
