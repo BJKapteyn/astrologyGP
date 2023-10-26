bf8f611e3e87caa8a264aced9ddc0afbc3d4920a
Neville Bamshoot <youcanteat@hotmail.com>
2023-10-25 20:10:49 -0400

    added modal styling

--- src/Components/PageElements/CardModal/CardModal.jsx
+++ src/Components/PageElements/CardModal/CardModal.jsx
@@ -1,27 +1,22 @@
+import './CardModal.css';
 
 export const CardModal = ({cardData, callBackDeselect}) => {
 
-    // // 
-    // function deSelect() {
-    //     selectBioData(null);
-    // }
-
-    // function select(bioData) {
-    //     if(bioData != null) 
-    //         selectBioData(bioData);
-    // }
-
     return (
-        <div className="cardmodal-background" onClick={callBackDeselect}>
+        <div>
             <section className="cardmodal-container">
-                <span onClick={callBackDeselect} className="cardmodal-exit">X</span>
-                <div className="cardmodal-biocard">
-                <img alt={cardData.description} src={require('../../../Pics/BioCardPics/' + cardData.image + '.jpg')} className="cardmodal-image"></img>
-                <div className="cardmodal-description">
-                    <p className="cardmodal-description-text">{cardData.description}</p>
-                </div>
+                <div className="cardmodal-card">
+                    <span onClick={callBackDeselect} className="cardmodal-exit">X</span>
+                    <div className="cardmodal-biocard">
+                        <img className="cardmodal-image" alt={cardData.description} src={require('../../../Pics/BioCardPics/' + cardData.image + '.jpg')}></img>
+                        <div className="cardmodal-description">
+                            <p className="cardmodal-description-text">{cardData.description}</p>
+                        </div>
+                    </div>
                 </div>
             </section>
+            <div className="cardmodal-background" onClick={callBackDeselect}></div>
+
         </div>
     );
 }
