3e64fbfd34fadbf1fd876c12285e960c34185d2a
Neville Bamshoot <youcanteat@hotmail.com>
2023-10-29 21:13:01 -0400

    started styling on biocard, changed nav bar fade color

--- src/Components/PageElements/BioCard/BioCards.jsx
+++ src/Components/PageElements/BioCard/BioCards.jsx
@@ -6,11 +6,12 @@ import { CardInfoData } from "./CardInfoData";
 export function BioCards() {
     const [bioData, selectBioData] = useState(null);
 
-    // 
+    // Deselects data for modal
     function deSelectBioCard() {
         selectBioData(null);
     }
-
+    
+    // Selects data for modal
     function selectBioCard(bioData) {
         if(bioData != null) 
             selectBioData(bioData);
@@ -18,19 +19,20 @@ export function BioCards() {
 
     return (
         <section className="biocard-section">
+            <div className="biocard-title-container">
+                <h1 id="biocard-title">Meet Y Us ABCDEFGHIJKLMNOP</h1>
+            </div>
             <div className="biocard-flex-container">
+            
                 {CardInfoData.map((card) => {
                     return <BioCard
                                 key={card.id} 
                                 callbackSelect={selectBioCard}
-                                // id={card.id} 
-                                // description={card.description} 
-                                // image={card.image} 
                                 cardData={card}
                                 />;
-                                
                     })}
             </div>
+            {/* This can deselect the modal */}
             {bioData !== null ? (<CardModal cardData={bioData} callBackDeselect={deSelectBioCard} ></CardModal>) : null}
         </section>
     );
--- src/Components/PageElements/BioCard/CardInfoData.jsx
+++ src/Components/PageElements/BioCard/CardInfoData.jsx
@@ -13,7 +13,10 @@ export const CardInfoData = [
     },
     {
         id: 3,
-        image: "BusinessGuy1",
-        description: "This person is fantastic"
+        image: "BusinessGuy2",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis quibusdam eaque ut minima labore! Recusandae atque obcaecati, dignissimos ullam molestiae voluptate, optio velit aperiam a, doloremque et. Doloribus, maxime suscipit!" + 
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis quibusdam eaque ut minima labore! Recusandae atque obcaecati, dignissimos ullam molestiae voluptate, optio velit aperiam a, doloremque et. Doloribus, maxime suscipit!" + 
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis quibusdam eaque ut minima labore! Recusandae atque obcaecati, dignissimos ullam molestiae voluptate, optio velit aperiam a, doloremque et. Doloribus, maxime suscipit!" + 
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis quibusdam eaque ut minima labore! Recusandae atque obcaecati, dignissimos ullam molestiae voluptate, optio velit aperiam a, doloremque et. Doloribus, maxime suscipit!"  
     },
 ]
