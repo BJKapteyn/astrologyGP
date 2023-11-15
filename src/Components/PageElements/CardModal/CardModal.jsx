480a3c65bf4c76a706c82af4b83b554864a4a0c0
Neville Bamshoot <youcanteat@hotmail.com>
2023-11-14 19:50:05 -0500

    changed image paths

--- src/Components/PageElements/CardModal/CardModal.jsx
+++ src/Components/PageElements/CardModal/CardModal.jsx
@@ -10,7 +10,7 @@ export const CardModal = ({cardData, callBackDeselect}) => {
                     <div className="cardmodal-card">
                         <span onClick={callBackDeselect} className="cardmodal-exit">X</span>
                         <div className="cardmodal-biocard">
-                            <img className="cardmodal-image" alt={cardData.description} src={require('../../../Pics/BioCardPics/' + cardData.image + '.jpg')}></img>
+                            <img className="cardmodal-image" alt={cardData.description} src={require('../../../Pics/Headshots/' + cardData.image)}></img>
                             <div className="cardmodal-description">
                                 <p className="cardmodal-description-text">{cardData.description}</p>
                             </div>
