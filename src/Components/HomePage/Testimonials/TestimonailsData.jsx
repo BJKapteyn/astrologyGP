8863be67e7b4685896400feaa74c8ac635dfc47d
Neville Bamshoot <youcanteat@hotmail.com>
2023-11-27 18:39:47 -0500

    added testimonial data

---
+++ src/Components/HomePage/Testimonials/TestimonailsData.jsx
@@ -0,0 +1,14 @@
+
+
+export default TestimonialsData = [
+    {
+        id: 1,
+        name: 'Angie B.',
+        testimonial: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis eaque magnam excepturi impedit nobis, enim odit aliquid minus distinctio cum accusamus voluptates nostrum quo eveniet ipsum, nisi voluptatum nihil ipsam.'
+    },
+    {
+        id: 1,
+        name: 'Jordan A.',
+        testimonial: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis eaque magnam excepturi impedit nobis, enim odit aliquid minus distinctio cum accusamus voluptates nostrum quo eveniet ipsum, nisi voluptatum nihil ipsam.'
+    }
+]
