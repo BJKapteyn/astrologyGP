27a76be9ce1f171080d95baf045d72f8383ea53f
Neville Bamshoot <youcanteat@hotmail.com>
2023-11-28 17:53:50 -0500

    general styling and formatting

--- src/Components/HomePage/Testimonials/Testimonials.jsx
+++ src/Components/HomePage/Testimonials/Testimonials.jsx
@@ -1,21 +1,33 @@
+import { TestimonialsData } from './TestimonialsData';
 import './Testimonials.css';
 
 export const Testimonials = () => {
     return (
         <div className='testimonials-section'>
             <section className='testimonials'>
-                <div className="testimonials-title">
+                <div className='testimonials-title'>
                     <h2>Testimonials</h2>
                 </div>
-                <div className="testimonials-flex-container">
+                <div className='testimonials-flex-container'>
+        {
+            TestimonialsData.slice(0, 2).map((testimonial) => {
+                const dataSource = testimonial.image == null ? require('../../../Pics/Portraits/portrait-airballoon.png') :
+                                                              require('../../../Pics/Headshots/' + testimonial.image);
+
+                return (
                     <div className='testimonials-flex-item'>
-                        <h3>Angie B.</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis eaque magnam excepturi impedit nobis, enim odit aliquid minus distinctio cum accusamus voluptates nostrum quo eveniet ipsum, nisi voluptatum nihil ipsam.</p>
+                        <div className='testimonials-header'>
+                            <div className='testimonials-image-container'>
+                                <img alt={testimonial.name} className='testimonials-image' src={dataSource}></img>
+                            </div>
+                            <h3 className='testimonials-name'>{testimonial.name}</h3>
+                        </div> 
+                        <p className='testimonials-testimonial'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis eaque magnam excepturi impedit nobis, enim odit aliquid minus distinctio cum accusamus voluptates nostrum quo eveniet ipsum, nisi voluptatum nihil ipsam.</p>
                     </div>
-                    <div className='testimonials-flex-item'>
-                        <h3>Jordan A.</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis eaque magnam excepturi impedit nobis, enim odit aliquid minus distinctio cum accusamus voluptates nostrum quo eveniet ipsum, nisi voluptatum nihil ipsam.</p>
-                    </div>
+
+                )
+            })
+        }
                 </div>
             </section>  
         </div>
---
+++ src/Components/HomePage/Testimonials/TestimonialsData.jsx
@@ -0,0 +1,14 @@
+export const TestimonialsData = [
+    {
+        id: 1
+        ,name: 'Angie B.'
+        ,testimonial: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis eaque magnam excepturi impedit nobis, enim odit aliquid minus distinctio cum accusamus voluptates nostrum quo eveniet ipsum, nisi voluptatum nihil ipsam.'
+        ,image: 'woman6.png'
+    },
+    {
+        id: 1
+        ,name: 'Jordan A.'
+        ,testimonial: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis eaque magnam excepturi impedit nobis, enim odit aliquid minus distinctio cum accusamus voluptates nostrum quo eveniet ipsum, nisi voluptatum nihil ipsam.'
+        ,image: null
+    }
+];
