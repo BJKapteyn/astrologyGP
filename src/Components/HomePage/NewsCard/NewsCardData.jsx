56d1683c22e7aeb785d25c807b4f7fc9722f7159
Neville Bamshoot <youcanteat@hotmail.com>
2023-11-28 19:23:00 -0500

    added data for news cards, changed page fonts

---
+++ src/Components/HomePage/NewsCard/NewsCardData.jsx
@@ -0,0 +1,34 @@
+export const NewsCardData = [
+    {
+        id: 1
+        ,image: 'portrait-girlsField.png'
+        ,text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde pariatur debitis sapiente animi cupiditate cumque ducimus tenetur aliquam excepturi incidunt blanditiis vel officia optio labore rerum, temporibus molestiae suscipit eveniet!'
+        ,imageLeftSide: true
+    },
+    {
+        id: 2
+        ,image: 'portrait-sunsetWaves.png'
+        ,text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde pariatur debitis sapiente animi cupiditate cumque ducimus tenetur aliquam excepturi incidunt blanditiis vel officia optio labore rerum, temporibus molestiae suscipit eveniet!'
+        ,imageLeftSide: true
+    },
+    {
+        id: 3
+        ,image: 'portrait-womanMountains.png'
+        ,text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde pariatur debitis sapiente animi cupiditate cumque ducimus tenetur aliquam excepturi incidunt blanditiis vel officia optio labore rerum, temporibus molestiae suscipit eveniet!'
+        ,imageLeftSide: true
+    },
+    {
+        id: 4
+        ,image: 'portrait-moonBeach.png'
+        ,text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde pariatur debitis sapiente animi cupiditate cumque ducimus tenetur aliquam excepturi incidunt blanditiis vel officia optio labore rerum, temporibus molestiae suscipit eveniet!'
+        ,imageLeftSide: true
+    },
+    {
+        id: 5
+        ,image: 'portrait-womainField3.png'
+        ,text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde pariatur debitis sapiente animi cupiditate cumque ducimus tenetur aliquam excepturi incidunt blanditiis vel officia optio labore rerum, temporibus molestiae suscipit eveniet!'
+        ,imageLeftSide: true
+    },
+
+
+]
