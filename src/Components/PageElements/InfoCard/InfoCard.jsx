fedd10607c49ea7df827ccf3da23f8474b1499f5
Neville Bamshoot <youcanteat@hotmail.com>
2023-12-04 16:18:37 -0500

    added joinus jsx file

--- src/Components/PageElements/InfoCard/InfoCard.jsx
+++ src/Components/PageElements/InfoCard/InfoCard.jsx
@@ -1,26 +1,28 @@
+import { JoinUs } from "./JoinUs";
 import {InfoCardData} from "./InfoCardData";
 import blurbimage from '../../../Pics/Landscapes/groupYoga.png';
+import blurbimage2 from '../../../Pics/Landscapes/findSign.png';
 import "./InfoCard.css";
 
 export function InfoCard() {
     return (
         <div>
             <div className="title-container">
-                <h1 id="infocard-title">WELCOME!</h1>
+                <h1 id="infocard-title">REMOTE DIVINATION READINGS</h1>
             </div>
             <div className="infocard-joinflex">
-                <div className="infocard-joinflexitem" id="infocard-blurbimage-container">
+                <div className="infocard-joinflexitem" id="infocard-leftimage-container">
                     <img src={blurbimage} alt="yoga" id="infocard-blurbimage" />
                 </div>
                 <div className="infocard-joinflexitem" id="infocard-join">
-                    <h2>JOIN US</h2>
+                    <h2>WELCOME!</h2>
                     <p className="infocard-description">Welcome to Vibe Collective, where we provide quality divination readings online to help you access various metaphysical and healing techniques. Our goal is to create an intimate and accessible user experience, so you can book your reading from anywhere in the world at any time of the day.</p>
                 </div>
-                <div className="infocard-joinflexitem" id="infocard-joinlink">
-                    <p className="infocard-description">a</p>
+                <div className="infocard-joinflexitem" id="infocard-rightimage-container">
+                    <img src={blurbimage2} alt="astrology" id="infocard-blurbimage" />
                 </div>
             </div>
-
+            <JoinUs />
             <div className="infocard-flex-container">
                 {InfoCardData.map((data) => {
                     const {id, image, keywordText} = data;
---
+++ src/Components/PageElements/InfoCard/JoinUs.jsx
@@ -0,0 +1,13 @@
+import girlsInField from '../../../Pics/Landscapes/girlsInField.png'
+
+export const JoinUs = () => {
+
+    return (
+        <section className="joinus">
+            <div className="joinus-header">
+                <img src={girlsInField} alt="" className="joinus-headerimage" />
+                <h1 className="joinus-headertext">JOIN OUR COLLECTIVE</h1>
+            </div>
+        </section>
+    );
+}
