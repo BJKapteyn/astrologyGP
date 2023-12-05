aa4f53507223638d2d17af3ad9679a081a80d4c5
Neville Bamshoot <youcanteat@hotmail.com>
2023-12-04 21:04:35 -0500

    added header for join information section

--- src/Components/PageElements/InfoCard/JoinUs.jsx
+++ src/Components/PageElements/InfoCard/JoinUs.jsx
@@ -5,8 +5,16 @@ export const JoinUs = () => {
     return (
         <section className="joinus">
             <div className="joinus-header">
-                <img src={girlsInField} alt="" className="joinus-headerimage" />
                 <h1 className="joinus-headertext">JOIN OUR COLLECTIVE</h1>
+                <img src={girlsInField} alt="women holding hands in a field" className="joinus-headerimage" />
+            </div>
+            <div className="joinus-flex">
+                <div className="joinus-flexitem" id="joinus-schedulemedium">
+                    <h1>
+
+                    </h1>
+                </div>
+                <div className="joinus-flexitem"></div>
             </div>
         </section>
     );
