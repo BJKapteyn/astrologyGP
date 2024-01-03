cbfa5bd37a132cb11ae2cd5ad778bc6a95c17fb1
Neville Bamshoot <youcanteat@hotmail.com>
2024-01-02 19:46:21 -0500

    added new logo, added about section

---
+++ src/Components/HomePage/AboutSection/AboutSection.jsx
@@ -0,0 +1,11 @@
+import { InfoCard } from '../../PageElements/InfoCard/InfoCard'
+import { InfoCardData } from '../../PageElements/InfoCard/InfoCardData'
+
+export const AboutSection = () => {
+    
+    return (
+        <section className="aboutsection">
+
+        </section>
+    )
+}
