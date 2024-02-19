4a1e9496795711a8bacecb083b6d096060eb779e
Neville Bamshoot <youcanteat@hotmail.com>
2024-02-19 17:15:32 -0500

    formatting

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import ScrollToTop from './ScrollToTop.jsx';
-import ReadersPage from './Pages/ReadersPage.jsx'
 import Layout from './Components/Layout/Layout.jsx';
+import ReadersPage from './Pages/ReadersPage.jsx'
 import HomePage from './Pages/HomePage.jsx';
 import NewsPage from './Pages/NewsPage.jsx';
 import './App.css';
@@ -12,15 +12,15 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-          <ScrollToTop>
-        <Routes>
+        <ScrollToTop>
+          <Routes>
             <Route path="/" element={<Layout />}>
               <Route index element={<HomePage />}></Route>
               <Route path="/Readers" element={<ReadersPage />}></Route>
               <Route path="/News" element={<NewsPage />}></Route>
             </Route>
-        </Routes>
-          </ScrollToTop>
+          </Routes>
+        </ScrollToTop>
       </BrowserRouter>
     </div>
   );
