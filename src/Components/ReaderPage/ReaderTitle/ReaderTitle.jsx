85955b9b0dd6bd0a1d4d1424ec1978b4725a5211
Neville Bamshoot <youcanteat@hotmail.com>
2024-04-05 12:38:56 -0400

    formatting on the whyvibe page

--- src/Components/ReaderPage/ReaderTitle/ReaderTitle.jsx
+++ src/Components/ReaderPage/ReaderTitle/ReaderTitle.jsx
@@ -4,12 +4,14 @@ import './ReaderTitle.css';
 export const ReaderTitle = () => {
     return (
         <section className="reader-section">
-            <h1 id="reader-title">Work with the Collective</h1>
-            <div className="reader-info">
-                <p className="reader-infotext">
-                    Are you yearning to reconnect with the souls who have touched your heart? Book a session with us, and together, we will open the door to the infinite wisdom and love that exists in the spirit realm. Whether you are seeking closure, understanding, or simply a reaffirmation of the eternal bond with your loved ones, I am here to guide you on this sacred journey.
-                    Thank you for entrusting me with your spiritual path. May the messages from the other side bring you comfort, healing, and the undeniable knowledge that love never dies.
-                </p>
+            <div className='reader-title-section'>
+                <h1 id="reader-title">Work with the Collective</h1>
+                <div className="reader-info">
+                    <p className="reader-infotext">
+                        Are you yearning to reconnect with the souls who have touched your heart? Book a session with us, and together, we will open the door to the infinite wisdom and love that exists in the spirit realm. Whether you are seeking closure, understanding, or simply a reaffirmation of the eternal bond with your loved ones, I am here to guide you on this sacred journey.
+                        Thank you for entrusting me with your spiritual path. May the messages from the other side bring you comfort, healing, and the undeniable knowledge that love never dies.
+                    </p>
+                </div>
             </div>
             <h3 className="reader-header">Terrot Card Readers</h3>
             <div className="reader-terot">
--- src/Components/WhyVibe/WhyVibe.jsx
+++ src/Components/WhyVibe/WhyVibe.jsx
@@ -1,25 +1,41 @@
 import './WhyVibe.css';
 export const WhyVibe = () => {
-
+    
     return (
         <section className="whyvibe">
-            <h1>why you should vibe</h1>
-            <p>At The Vibe Collective, we don't just offer jobs; we craft careers within a dynamic and inclusive ecosystem. We're committed to fostering growth, embracing collaboration, and achieving a harmonious work-life balance.</p>
-            <h2>Why Choose The Vibe Collective?</h2>
-            <h3>Recognition:</h3>
-            <p>Your talents won't go unnoticed here. We actively recognize and nurture the unique strengths you bring to the table, ensuring you flourish in your professional journey.</p>
-            <h3>Team Commitment:</h3>
-            <p>We're more than just a team; we're collaborators on a shared mission. Join a collective committed to expansion, where every voice is heard, and every idea contributes to our shared success.</p>
-            <h3>Global Impact:</h3>
-            <p>Your work at The Vibe Collective isn't confined by borders. Join us to make a difference worldwide and be part of initiatives that transcend geographical boundaries.</p>
-            <h3>Competitive Compensation:</h3>
-            <p>We understand the value of your skills. Enjoy not just an hourly wage, but a competitive compensation package that reflects your expertise and dedication.</p>
-            <h3>Comprehensive Benefits:</h3>
-            <p>Your well-being matters. Benefit from a comprehensive package that includes health, financial wellness, and professional development perks, ensuring you thrive both personally and professionally.</p>
-            <h3>Valued and Appreciated:</h3>
-            <p>At The Vibe Collective, you're not just an employee; you're a valued member. Your contributions are not only appreciated but celebrated as essential elements of our collective success.</p>
-            <h3>Aspirations Supported:</h3>
-            <p>Your dreams are our priority. We actively support your aspirations, providing the resources and encouragement needed for you to reach new heights in your career.</p>
+            <div className="whyvibe-title">
+                <h1>why you should vibe</h1>
+                <p>At The Vibe Collective, we don't just offer jobs; we craft careers within a dynamic and inclusive ecosystem. We're committed to fostering growth, embracing collaboration, and achieving a harmonious work-life balance.</p>
+            </div>
+            <h2>Why Choose The Vibe Collective?</h2>\
+            <div className="whyvibe-bullet-flex">
+                <h3 className="whyvibe-bullet">Recognition:</h3>
+                <p className="whyvibe-text">Your talents won't go unnoticed here. We actively recognize and nurture the unique strengths you bring to the table, ensuring you flourish in your professional journey.</p>
+            </div>
+            <div className="whyvibe-bullet-flex">
+                <h3 className="whyvibe-bullet">Team Commitment:</h3>
+                <p className="whyvibe-text">We're more than just a team; we're collaborators on a shared mission. Join a collective committed to expansion, where every voice is heard, and every idea contributes to our shared success.</p>
+            </div>
+            <div className="whyvibe-bullet-flex">
+                <h3 className="whyvibe-bullet">Global Impact:</h3>
+                <p className="whyvibe-text">Your work at The Vibe Collective isn't confined by borders. Join us to make a difference worldwide and be part of initiatives that transcend geographical boundaries.</p>
+            </div>
+            <div className="whyvibe-bullet-flex">
+                <h3 className="whyvibe-bullet">Competitive Compensation:</h3>
+                <p className="whyvibe-text">We understand the value of your skills. Enjoy not just an hourly wage, but a competitive compensation package that reflects your expertise and dedication.</p>
+            </div>
+            <div className="whyvibe-bullet-flex">
+                <h3 className="whyvibe-bullet">Comprehensive Benefits:</h3>
+                <p className="whyvibe-text">Your well-being matters. Benefit from a comprehensive package that includes health, financial wellness, and professional development perks, ensuring you thrive both personally and professionally.</p>
+            </div>
+            <div className="whyvibe-bullet-flex">
+                <h3 className="whyvibe-bullet">Valued and Appreciated:</h3>
+                <p className="whyvibe-text">At The Vibe Collective, you're not just an employee; you're a valued member. Your contributions are not only appreciated but celebrated as essential elements of our collective success.</p>
+            </div>
+            <div className="whyvibe-bullet-flex">
+                <h3 className="whyvibe-bullet">Aspirations Supported:</h3>
+                <p className="whyvibe-text">Your dreams are our priority. We actively support your aspirations, providing the resources and encouragement needed for you to reach new heights in your career.</p>
+            </div>
             <h4>Join The Vibe Collective and experience a workplace where diversity is celebrated, talents are nurtured, and every day is an opportunity to make a positive impact. Might be a great time to vibe</h4>
         </section>
     )
