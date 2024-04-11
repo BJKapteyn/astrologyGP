122bbfe38b1590635a853a3b6dfe2fee22ce531d
Neville Bamshoot <youcanteat@hotmail.com>
2024-04-11 15:31:43 -0400

    Added navbar and submenu data

--- src/Components/Layout/Navbar/NavLinkData.jsx
+++ src/Components/Layout/Navbar/NavLinkData.jsx
@@ -2,17 +2,73 @@
 export const NavLinkData = [
     {
         id: 1,
-        href: '',
-        text: 'HOME'
+        route: '/',
+        text: 'Home',
+        subMenu: null
     },
     {
         id: 2,
-        href: '',
-        text: 'BLOG'
+        route: '/WhyVibe',
+        text: 'Why Vibe?',
+        subMenu: null
     },
     {
         id: 3,
-        href: '',
-        text: 'STORE'
+        route: '/Readers',
+        text: 'Work With Us',
+        subMenu: null
+    },
+    {
+        id: 4,
+        route: '/',
+        text: 'Offerings',
+        subMenu: [
+            {
+                id: 6,
+                route: '',
+                text: 'Tarot',
+            },
+            {
+                id: 7,
+                route: '',
+                text: 'Astrology',
+            },
+            {
+                id: 8,
+                route: '',
+                text: 'Mediumship',
+            },
+            {
+                id: 9,
+                route: '',
+                text: 'Past Life Regression',
+            },
+            {
+                id: 10,
+                route: '',
+                text: 'Reiki',
+            },
+            {
+                id: 11,
+                route: '',
+                text: 'Human Design',
+            },
+            {
+                id: 12,
+                route: '',
+                text: 'Sound Healing',
+            },
+            {
+                id: 13,
+                route: '',
+                text: 'Kundalini Yoga',
+            },
+        ]
+    },
+    {
+        id: 5,
+        route: '/News',
+        text: 'The Current Vibe',
+        subMenu: null
     },
 ]
