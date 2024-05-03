f8d32e975d40ac1a82477667b253a606d83b9830
Neville Bamshoot <youcanteat@hotmail.com>
2024-05-02 21:26:30 -0400

    added css rules

--- src/Components/Layout/Navbar/NavBar.jsx
+++ src/Components/Layout/Navbar/NavBar.jsx
@@ -1,15 +1,27 @@
-import { Link } from "react-router-dom";
+import { NavLinkData } from "./NavLinkData";
+import { NavPageLink } from "./NavPageLink";
 import './NavBar.css';
 import '../../../App.css'
 
 export const NavBar = () => {
     return (
         <nav className='navbar'>
-            <Link className="navbar-link" to="/">Home</Link>
-            <Link className="navbar-link" to="/WhyVibe">Why Vibe?</Link>
-            <Link className="navbar-link" to="/WhyVibe">Offerings</Link>
-            <Link className="navbar-link" to="/Readers">Work With Us</Link>
-            <Link className="navbar-link" to="/News">The Current Vibe</Link>
+            {NavLinkData.map((linkDatas) => {
+                return (
+                    <ul>
+                        <li>
+                            <NavPageLink linkData={linkDatas}></NavPageLink>
+                        </li>
+                    </ul>
+                );
+            })}
         </nav>
+        // <nav className='navbar'>
+        //     <Link className="navbar-link" to="/">Home</Link>
+        //     <Link className="navbar-link" to="/WhyVibe">Why Vibe?</Link>
+        //     <Link className="navbar-link" to="/WhyVibe">Offerings</Link>
+        //     <Link className="navbar-link" to="/Readers">Work With Us</Link>
+        //     <Link className="navbar-link" to="/News">The Current Vibe</Link>
+        // </nav>
     )
 }
