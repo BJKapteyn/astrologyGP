38b8f6e1fc5be5ed780ed0e0a48026000ae3197b
Neville Bamshoot <youcanteat@hotmail.com>
2024-05-16 06:57:37 -0400

    added hover effect on navbar

--- src/Components/Layout/Navbar/NavBar.jsx
+++ src/Components/Layout/Navbar/NavBar.jsx
@@ -9,17 +9,10 @@ export const NavBar = () => {
             <ul className="navbar-main-nav">
                 {NavLinkData.map((linkDatas) => {
                     return (
-                        <NavPageLink linkData={linkDatas}></NavPageLink>
+                        <NavPageLink key={linkDatas.id} linkData={linkDatas}></NavPageLink>
                     );
                 })}
             </ul>
         </nav>
-        // <nav className='navbar'>
-        //     <Link className="navbar-link" to="/">Home</Link>
-        //     <Link className="navbar-link" to="/WhyVibe">Why Vibe?</Link>
-        //     <Link className="navbar-link" to="/WhyVibe">Offerings</Link>
-        //     <Link className="navbar-link" to="/Readers">Work With Us</Link>
-        //     <Link className="navbar-link" to="/News">The Current Vibe</Link>
-        // </nav>
     )
 }
--- src/Components/Layout/Navbar/NavPageLink.jsx
+++ src/Components/Layout/Navbar/NavPageLink.jsx
@@ -1,18 +1,25 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 // return Link with data or submenu if it exists
 export const NavPageLink = ({linkData}) => {
     const {text, route, subMenu} = linkData;
     const link = <Link className="navbar-link" to={route}>{text}</Link>;
+    const [isHovering, setIsHovering] = useState(false);
+
     let navLink;
 
     if(subMenu) {
         navLink = (
-            <ul className="navbar-submenu">
+            <ul 
+            onMouseEnter={() => setIsHovering(true)}
+            onMouseLeave={() => setIsHovering(false)}
+            className="navbar-submenu"
+            >
                 <li className="navbar-submenu-children" id="navbar-submenu-top">
                     {link}
                 </li>
-                {subMenu.map((subMenu) => {
+                {isHovering && subMenu.map((subMenu) => {
                     return (
                         <li key={subMenu.id} className="navbar-submenu-children">
                             <Link role="link" className="navbar-link" to={subMenu.route}>{subMenu.text}</Link>
