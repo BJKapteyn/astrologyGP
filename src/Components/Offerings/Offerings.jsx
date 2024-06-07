727b41d190a58fb9e540daf73fc368c3aa59d2e9
Neville Bamshoot <youcanteat@hotmail.com>
2024-06-06 21:24:12 -0400

    added setup code for offerings page and getting item data

---
+++ src/Components/Offerings/Offerings.jsx
@@ -0,0 +1,10 @@
+
+
+export const Offerings = () => {
+    
+    return (
+        <section>
+            
+        </section>
+    )
+}
---
+++ src/Pages/Offerings.jsx
@@ -0,0 +1,10 @@
+import { useEffect } from "react"
+
+export const Offerings = () => {
+    
+    return (
+        <section>
+            
+        </section>
+    )
+}
