3303b17ecad3ac48e542bd24b89a080b8a17893f
Neville Bamshoot <youcanteat@hotmail.com>
2024-06-17 20:06:37 -0400

    added simple display for items

--- src/Components/Offerings/Items/Items.jsx
+++ src/Components/Offerings/Items/Items.jsx
@@ -1,13 +1,29 @@
+import '../../../App.css';
 import './Items.css';
+import moon from '../../../Pics/Landscapes/moonClouds.png';
 
 export const Items = ({ itemData }) => {
 
     return (
-        <ul>
+        <div className="items-flex">
             {itemData.map(item => {
+                const itemObject = item.ItemData;
 
-               return <li key={item.Id}>{item.ItemData.Name}</li>;
+                return (
+                        <div key={item.Id} className="items-item">
+                            <div className="items-image-container">
+                                <img className="items-image" src={moon} alt="" />
+                            </div>
+                            <h4 className="items-name">{itemObject.Name}</h4>
+                            <div className="items-description">
+                                <p className="items-descriptiontext">{itemObject.Description}</p>
+                            </div>
+                            <div className="items-booknowbutton">
+                                <button className="items-booknow">Book Now</button>
+                            </div>
+                        </div>
+                );      
             }, [])}
-        </ul>
+        </div >
     );
 }
