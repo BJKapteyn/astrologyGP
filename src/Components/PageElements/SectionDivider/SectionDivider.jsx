4e2c75e4ab8a1c1ad083a87eb62459431128537c
Neville Bamshoot <youcanteat@hotmail.com>
2024-06-20 22:58:42 -0400

    added favicon and changed website title

---
+++ src/Components/PageElements/SectionDivider/SectionDivider.jsx
@@ -0,0 +1,8 @@
+export const SectionDivider = ({text}) => {
+
+    return (
+        <section className="divider">
+            <h1 className="divider-text">{text}</h1>
+        </section>
+    );
+}
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,5 +1,4 @@
 import { TitleCard } from '../Components/HomePage/TitleCard/TitleCard.jsx';
-import { InfoCard } from '../Components/PageElements/InfoCard/InfoCard';
 import { NewsCardSection } from '../Components/HomePage/NewsCard/NewsCardSection.jsx';
 import { Testimonials } from '../Components/HomePage/Testimonials/Testimonials.jsx';
 import { JoinUs } from '../Components/HomePage/JoinUs/JoinUs.jsx';
@@ -7,12 +6,14 @@ import { Subscribe } from '../Components/HomePage/Subscribe/Subscribe.jsx';
 import { RecruitSection } from '../Components/HomePage/Recruit/RecruitSection.jsx';
 import { AboutSection } from '../Components/HomePage/AboutSection/AboutSection.jsx';
 import { Book } from '../Components/HomePage/Book/Book.jsx';
+import { SectionDivider } from '../Components/PageElements/SectionDivider/SectionDivider.jsx';
 
 export default function HomePage() {
 
     return (
         <div className="scroll-snap">
             <TitleCard></TitleCard>
+            <SectionDivider textData={['tarot readings', 'astrology', 'mediumship']} />
             <JoinUs></JoinUs>
             {/* <InfoCard></InfoCard> */}
             <NewsCardSection></NewsCardSection>
