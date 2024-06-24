077471f939ebc00b7c566fcbeb8bceafda05f54b
Neville Bamshoot <youcanteat@hotmail.com>
2024-06-24 19:27:27 -0400

    changed name from recruit to offering section on homepage

--- src/Components/HomePage/OfferingsSection/OfferingItem.jsx
+++ src/Components/HomePage/OfferingsSection/OfferingItem.jsx
@@ -1,20 +1,20 @@
 import { ActionButton } from "../../PageElements/ActionButton/ActionButton";
-import './OfferingSection.css';
+import './OfferingsSection.css';
 
 export const OfferingItem = ({offeringItemData}) => {
-    const {id, title, text, image} = offeringItemData;
+    const {title, text, image} = offeringItemData;
 
     const imageSource = image ? require('../../../Pics/Landscapes/' + image) : null;
 
     return (
-        <div className="offering-section--flexbox">
-            <div className="offering-section--title-container">
-                <h3 className="offering-section--title">{title}</h3>
+        <div className="offering-section-flexbox">
+            <div className="offering-section-title-container">
+                <h3 className="offering-section-title">{title}</h3>
             </div>
-            <div className="offering-section--image-container">
-                <img className="offering-section--image" src={imageSource} alt={title} />
+            <div className="offering-section-image-container">
+                <img className="offering-section-image" src={imageSource} alt={title} />
             </div>
-            <div className="offering-section--text-container">
+            <div className="offering-section-text-container">
                 <p>{text}</p>
             </div>
             <ActionButton buttonSettings={{buttonText: 'Join Waitlist'}}></ActionButton>
--- src/Components/HomePage/OfferingsSection/OfferingsSection.jsx
+++ src/Components/HomePage/OfferingsSection/OfferingsSection.jsx
@@ -1,14 +1,15 @@
 import { OfferingSectionData } from './OfferingSectionData.jsx';
 import { OfferingItem } from './OfferingItem.jsx';
+import './OfferingsSection.css'
 
 export const OfferingSection = () =>  {
     
     return (
-        <section className="offering-section-section">
+        <section className="offering-sectionmain">
             <div className="offering-section-flex">
                 {OfferingSectionData.map(data => {
                     return (
-                        <OfferingItem key={data.id} recruitData={data}></OfferingItem>
+                        <OfferingItem key={data.id} offeringItemData={data}></OfferingItem>
                     );
                 })}
             </div>
