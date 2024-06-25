5a9b1bc6d130c298f8b88a9a04a6ce72a0179313
Neville Bamshoot <youcanteat@hotmail.com>
2024-06-24 20:21:19 -0400

    restyled offeringsection

--- src/Components/HomePage/OfferingsSection/OfferingItem.jsx
+++ src/Components/HomePage/OfferingsSection/OfferingItem.jsx
@@ -1,21 +1,19 @@
 import { ActionButton } from "../../PageElements/ActionButton/ActionButton";
+import '../../../App.css';
 import './OfferingsSection.css';
 
 export const OfferingItem = ({offeringItemData}) => {
-    const {title, text, image} = offeringItemData;
+    const {style, title, text, image} = offeringItemData;
 
     const imageSource = image ? require('../../../Pics/Landscapes/' + image) : null;
 
     return (
         <div className="offering-section-flexbox">
-            <div className="offering-section-title-container">
+            <div id={style} className="offering-section-title-container">
                 <h3 className="offering-section-title">{title}</h3>
             </div>
-            <div className="offering-section-image-container">
-                <img className="offering-section-image" src={imageSource} alt={title} />
-            </div>
             <div className="offering-section-text-container">
-                <p>{text}</p>
+                <p className="offering-section-text">{text}</p>
             </div>
             <ActionButton buttonSettings={{buttonText: 'Join Waitlist'}}></ActionButton>
         </div>
