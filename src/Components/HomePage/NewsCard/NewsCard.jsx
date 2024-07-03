de8135abaeaa24df1c22fc0b712969997f735693
Neville Bamshoot <youcanteat@hotmail.com>
2024-07-03 13:24:26 -0400

    started newscard mobile design

--- src/Components/HomePage/NewsCard/NewsCard.jsx
+++ src/Components/HomePage/NewsCard/NewsCard.jsx
@@ -8,15 +8,16 @@ export const NewsCard = ({newsCardData}) => {
     const imagePath = require('../../../Pics/' + newsCardData.image);
     const imageId = 'newscard-image' + newsCardData.id;
 
-    const imageAndContainer = (
+    const image = (
         <div className="newscard-image-container">
             <img id={imageId} src={imagePath} alt={newsCardData.id} className="newscard-image" />
         </div>
     );
 
-    const textAndContainer = (
+    const text = (
         <div className="newscard-text-container">
             <div className="newscard-crop">
+                <div id="newscard-textCover"></div>
                 <h3 className="newscard-text-title">{newsCardData.title}</h3>
                 <div className="newscard-text-crop">
                     <p className="newscard-text">{newsCardData.text}</p>
@@ -25,12 +26,19 @@ export const NewsCard = ({newsCardData}) => {
         </div>
     );
 
-    // set order of image and text
+    // Set order of image and text
     const newsCard = (
         <div key={newsCardData.id} className="newscard">
-            {newsCardData.imageLeftSide ? imageAndContainer : textAndContainer}
-            {newsCardData.imageLeftSide ? textAndContainer : imageAndContainer}
+            {newsCardData.imageLeftSide ? image : text}
+            {newsCardData.imageLeftSide ? text : image}
         </div>
     );
+
+    const newsCardMobile = (
+        <div key={newsCardData.id} className="newscard">
+            {image}
+            {text}
+        </div>
+    )
     return newsCard;
 }
