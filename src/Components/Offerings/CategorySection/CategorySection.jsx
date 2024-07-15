3a5c4840b29f921b686285625653f1f7de4fc1f3
Neville Bamshoot <youcanteat@hotmail.com>
2024-07-14 22:41:49 -0400

    styled whyvibe page for mobile

--- src/Components/Offerings/CategorySection/CategorySection.jsx
+++ src/Components/Offerings/CategorySection/CategorySection.jsx
@@ -2,11 +2,11 @@ import { useEffect, useState } from "react";
 import { Items } from "../Items/Items";
 import { TitleCard } from "../../PageElements/TitleCard/TitleCard";
 import { SectionDivider } from "../../PageElements/SectionDivider/SectionDivider";
+import { OfferingCategory } from "../data/OfferingCategory";
 import backgroundImage from '../../../Pics/Landscapes/tableHand_dark.png'
 import './Category.css';
 
 export const CategorySection = ({ productData }) => {
-    const [categories, setCategories] = useState([]);
     const [items, setItems] = useState([]);
     const categoryTitleCardData = {
         titleText: 'OFFERINGS',
@@ -16,30 +16,27 @@ export const CategorySection = ({ productData }) => {
 
 
     useEffect(() => {
-        let categoryData = [];
         let itemData = [];
 
         if(productData.length >= 0) {
             productData.forEach(data => {
-                if(data?.Type === 'CATEGORY')
-                    categoryData.push(data);
-        
+
                 if(data?.Type === 'ITEM')
                     itemData.push(data);
+                
             }, [productData]);
         }
         
-        setCategories(categoryData);
         setItems(itemData);
     }, [productData]);
 
     return (
         <>
             <TitleCard titleData={categoryTitleCardData}></TitleCard>
-            {categories.map(category => {
+            {OfferingCategory.map(category => {
                 return (
                     <section key={category.Id} className="category-section">
-                        <SectionDivider dividerData={{headingText: category.CategoryData.Name}}></SectionDivider>
+                        <SectionDivider dividerData={{headingText: category}}></SectionDivider>
 
                         <Items key={category.Id} itemData={items}></Items>
 
