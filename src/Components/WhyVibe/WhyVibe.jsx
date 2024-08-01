5b42350f184f6d695209b4d2c9c4754ee74ed543
Neville Bamshoot <youcanteat@hotmail.com>
2024-08-01 11:37:47 -0400

    tweaking video

--- src/Components/WhyVibe/WhyVibe.jsx
+++ src/Components/WhyVibe/WhyVibe.jsx
@@ -1,8 +1,8 @@
 import { WhyVibeCardData } from './WhyVibeCardData.jsx';
 import { ValueCards } from './ValueCards.jsx';
 import { TitleCard } from '../PageElements/TitleCard/TitleCard.jsx';
-import { VideoSection } from './VideoSection/VideoSection.jsx';
 import { CareerSection } from './CareerSection/CareerSection.jsx';
+import { VideoSection } from '../PageElements/VideoSection/VideoSection.jsx';
 import backgroundImage from '../../Pics/Landscapes/moonFade.png';
 
 import './WhyVibe.css';
@@ -13,10 +13,12 @@ export const WhyVibe = () => {
         styleId: 'whyvibe-titlecard'
     };
 
+    const videoSource = ''
+
     return (
         <main className="whyvibe">
             <TitleCard titleData={titleData}></TitleCard>
-            <VideoSection></VideoSection>
+            <VideoSection videoSource={videoSource}></VideoSection>
             <CareerSection></CareerSection>
             <ValueCards cardDatas={WhyVibeCardData}></ValueCards>
         </main>
