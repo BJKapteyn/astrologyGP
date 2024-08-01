e78405b2fdaaed182603a1bd85cdb79985ce8eec
Neville Bamshoot <youcanteat@hotmail.com>
2024-08-01 12:24:11 -0400

    fixed values on whyvibe page

--- src/Components/WhyVibe/CareerSection/CareerSection.jsx
+++ src/Components/WhyVibe/CareerSection/CareerSection.jsx
@@ -1,14 +1,17 @@
+import { Link } from 'react-router-dom';
 import './CareerSection.css';
 
 export const CareerSection = () => {
 
     return(
         <section className="careersection">
-                <h3 className="career-title">CAREERS AT THE VIBE COLLECTIVE</h3>
+                <h3 className="career-title">WORK WITH US</h3>
                 <div className="">
-                    <p className="career-text">If you are ready to elevate your career and enjoy being part of a vibrant, energetc team, we invite you to consider The Vibe Collective.</p>
-                    <p className="career-text">Click here to sign up for a confidential discussion</p>
-                    <p className="career-text">Questions? Call us at 555.457.8884, or email us at contactus@thevibecollective.co.</p>
+                    <p className="career-text">Are you ready to elevate your career and thrive within a vibrant, energetic team?</p>
+                    <p className="career-text">Join The Vibe Collective, where passion meets opportunity!</p>
+                    <p className="career-text">Send your resume and cover letter to:</p>
+                    <p className="career-text"><Link to={'mailto:info@thevibecollective.co'}>info@thevibecollective.co</Link></p>
+                    <p className="career-text">Take the first step toward an exciting future with us.</p>
                 </div>
         </section>
     )
