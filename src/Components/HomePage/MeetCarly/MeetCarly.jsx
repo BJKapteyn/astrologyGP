6ddc7eee1d7140db06507c7627c5103073870926
Neville Bamshoot <youcanteat@hotmail.com>
2024-08-01 23:54:29 -0400

    changed videos to v3

--- src/Components/HomePage/MeetCarly/MeetCarly.jsx
+++ src/Components/HomePage/MeetCarly/MeetCarly.jsx
@@ -7,7 +7,7 @@ export const MeetCarly = () => {
         buttonStyleId: 'meet-morebutton',
         action: null
     }
-    const vidSource = 'https://vibecollectivevideos.blob.core.windows.net/vibevideos/TVC Welcome V2 CAP.mp4';
+    const vidSource = 'https://vibecollectivevideos.blob.core.windows.net/vibevideos/TVCWelcomeV3CAPTIONS.mp4';
 
     return (
         <VideoSection videoSource={vidSource} buttonSettings={buttonSettings}></VideoSection>
