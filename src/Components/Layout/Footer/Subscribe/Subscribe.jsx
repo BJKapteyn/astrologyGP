ea9b6594de1dc3b9c0de63ea40a56d50dcbff952
Neville Bamshoot <youcanteat@hotmail.com>
2024-08-04 15:20:08 -0400

    hooked up email form to square

--- src/Components/Layout/Footer/Subscribe/Subscribe.jsx
+++ src/Components/Layout/Footer/Subscribe/Subscribe.jsx
@@ -1,4 +1,5 @@
 import { ActionButton } from '../../../PageElements/ActionButton/ActionButton';
+import { EmailForm } from './EmailForm';
 import './Subscribe.css';
 
 export const Subscribe = () => {
@@ -18,12 +19,7 @@ export const Subscribe = () => {
                             <h4 className="subscribe-description">Subscribe to our monthly newsletter:</h4>
                             <p className="subscribe-descriptiontext">Be the first to learn about special offers, seasonal classes, new services, and events.</p>
                         </div>
-                        <div className="subscribe-button">
-                            <input type="text" className="subscribe-textinput" id="submitemail" placeholder="Enter Your Email..."></input>
-
-                            <ActionButton  buttonSettings={subscribeButtonSettings}></ActionButton>
-                            
-                        </div>
+                        <EmailForm></EmailForm>
                         <p className="subscribe-subtext">We respect your privacy and your inbox.</p>
                     </div>
                 </div>
