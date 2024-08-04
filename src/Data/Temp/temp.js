07afdff77022b306632be26fe9c2d60f2687019d
Neville Bamshoot <youcanteat@hotmail.com>
2024-08-04 15:20:08 -0400

    hooked up email form to square

--- src/Data/Temp/temp.js
+++ src/Data/Temp/temp.js
@@ -1,5 +1,14 @@
-export let x4tt116 = 'JtUw3SPDapgS1i4TP0A9ze0CXKgqguB-smzsJ8UC2y-eAzFukFFLww==';
+export const x4tt116 = '?code=JtUw3SPDapgS1i4TP0A9ze0CXKgqguB-smzsJ8UC2y-eAzFukFFLww==';
+export const x45tt1337 = '?code=ih52hkIj4ZvxC2VwDNkynujLg_MdLo3SD7NYjuOYeDRrAzFukyqlAg%3D%3D';
+
 export function buildIt(str) {
-    let url = () => 'https://vibecollective.azurewebsites.net/api/GetItems?code=' + str;
-    return url;;
+    let url = () => 'https://vibecollective.azurewebsites.net/api/GetItems' + str;
+    
+    return url;
+}
+
+export function buildIt2() {
+    let url = 'https://vibecollective.azurewebsites.net/api/InsertCustomerEmail' + x45tt1337;
+
+    return url;
 }
