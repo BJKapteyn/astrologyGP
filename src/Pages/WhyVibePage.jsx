8d60bafba240ee27d46a265d8eee7e91d02cbc3d
Neville Bamshoot <youcanteat@hotmail.com>
2024-08-11 19:39:28 -0400

    added jpgs for all image files

--- src/Pages/WhyVibePage.jsx
+++ src/Pages/WhyVibePage.jsx
@@ -2,6 +2,11 @@ import { WhyVibe } from "../Components/WhyVibe/WhyVibe"
 
 export default function WhyVibePage() {
     return (
-        <WhyVibe></WhyVibe>
+        <main className="whyvibe">
+            <meta name="author" content="Brad Kapteyn"></meta>
+            <meta name="description" content="Join The Vibe Collective as an Astrology Medium or Tarot Reader"></meta>
+            <meta name="keywords" content="The Vibe Collective, astrology, readers, mediums"></meta>
+            <WhyVibe></WhyVibe>
+        </main>
     )
 }
