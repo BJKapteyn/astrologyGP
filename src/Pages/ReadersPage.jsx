9204e7b25d14c73a7198e3e25e462ede677b6d58
Neville Bamshoot <youcanteat@hotmail.com>
2024-08-12 20:52:09 -0400

    properly added head tags

--- src/Pages/ReadersPage.jsx
+++ src/Pages/ReadersPage.jsx
@@ -3,9 +3,9 @@ import { ReaderTitle } from '../Components/ReaderPage/ReaderTitle/ReaderTitle.js
 export default function ReadersPage() {
     return (
         <main>
-            <meta name="author" content="Brad Kapteyn"></meta>
+            {/* <meta name="author" content="Brad Kapteyn"></meta>
             <meta name="description" content="Coming soon, The Vibe Collective Astrological mediums and tarot card readers"></meta>
-            <meta name="keywords" content="The Vibe Collective, astrology, readers, mediums"></meta>
+            <meta name="keywords" content="The Vibe Collective, astrology, readers, mediums"></meta> */}
             <ReaderTitle></ReaderTitle>
         </main>
     )
--- src/Pages/WhyVibePage.jsx
+++ src/Pages/WhyVibePage.jsx
@@ -1,11 +1,18 @@
-import { WhyVibe } from "../Components/WhyVibe/WhyVibe"
+import { useEffect } from "react";
+import { addPageHeaderInfo } from "../Functions/pageUtility";
+import { WhyVibe } from "../Components/WhyVibe/WhyVibe";
 
 export default function WhyVibePage() {
+
+    useEffect(() => {
+        const descriptionText = 'Join The Vibe Collective as an Astrology Medium or Tarot Reader';
+        const keywordsText = 'The Vibe Collective, astrology, readers, mediums';
+
+        addPageHeaderInfo(null, descriptionText, keywordsText, null);
+    });
+
     return (
         <main className="whyvibe">
-            <meta name="author" content="Brad Kapteyn"></meta>
-            <meta name="description" content="Join The Vibe Collective as an Astrology Medium or Tarot Reader"></meta>
-            <meta name="keywords" content="The Vibe Collective, astrology, readers, mediums"></meta>
             <WhyVibe></WhyVibe>
         </main>
     )
