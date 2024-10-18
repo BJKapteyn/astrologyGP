05658ad854b89e99a42c834b0968399d890d3db4
Neville Bamshoot <youcanteat@hotmail.com>
2024-10-18 14:47:36 -0400

    added loading indicator component

---
+++ src/Components/PageElements/LoadingIndicator/LoadingIndicator.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './LoadingIndicator.css'; // Import the CSS file
+
+export const LoadingIndicator = () => {
+  return (
+    <p className="loading-text">Loading...</p>
+  );
+};
