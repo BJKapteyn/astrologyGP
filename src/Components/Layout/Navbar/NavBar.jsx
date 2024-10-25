bb64a18e769c9f7d275fd87dacf96059a78941f6
Neville Bamshoot <youcanteat@hotmail.com>
2024-10-24 20:35:02 -0400

    fixed nav bar to be centered

--- src/Components/Layout/Navbar/NavBar.jsx
+++ src/Components/Layout/Navbar/NavBar.jsx
@@ -8,19 +8,31 @@ import './NavBar.css';
 
 export const NavBar = () => {
     const isMobile = useMediaQuery({ query: '(max-width: 600px)' });
-
+    
     const defaultNavBar = (
         <nav className='navbar'>
             <ul className="navbar-main-nav">
-                <NavPageLink linkData={{text: 'HOME', route: '/', submenu: null}} />
-                <NavPageLink linkData={{text: 'OFFERINGS', route: '/Offerings', submenu: null}} />
-                <NavPageLink linkData={{text: 'THE COLLECTIVE', route: '/TheCollective', submenu: null}} />
+                <li className="navbar-child">
+                    <NavPageLink linkData={{text: 'HOME', route: '/', submenu: null}} />
+                </li>
+                <li className="navbar-child">
+                    <NavPageLink linkData={{text: 'OFFERINGS', route: '/Offerings', submenu: null}} />
+                </li>
+                <li className="navbar-child">
+                    <NavPageLink linkData={{text: 'THE COLLECTIVE', route: '/TheCollective', submenu: null}} />
+                </li>
                 <li className="navbar-logo-container">
                     <img alt='Yellow main logo' src={yellowLogo} className="navbar-logo" />
                 </li>
-                <NavPageLink linkData={{text: 'SHOP THE VIBE', route: '/Store', submenu: null}} />
-                <NavPageLink linkData={{text: 'THE CURRENT VIBE', route: '/CurrentVibe', submenu: null}} />
-                <NavPageLink linkData={{text: 'WORK WITH US', route: '/WhyVibe', submenu: null}} />
+                <li className="navbar-child">
+                    <NavPageLink linkData={{text: 'SHOP THE VIBE', route: '/Store', submenu: null}} />
+                </li>
+                <li className="navbar-child">
+                    <NavPageLink linkData={{text: 'THE CURRENT VIBE', route: '/CurrentVibe', submenu: null}} />
+                </li>
+                <li className="navbar-child">
+                    <NavPageLink linkData={{text: 'WORK WITH US', route: '/WhyVibe', submenu: null}} />
+                </li>
             </ul>
             <div className="navbar-social-flex">
                 <Socials></Socials>
