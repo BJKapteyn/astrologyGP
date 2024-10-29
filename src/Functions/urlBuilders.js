37f68ec0c6e8480e5ba4f82168341ecb396f2c24
Neville Bamshoot <youcanteat@hotmail.com>
2024-10-28 21:01:38 -0400

    new urlbuilder function

---
+++ src/Functions/urlBuilders.js
@@ -0,0 +1,5 @@
+export function buildSingleOfferingURL(itemID) {
+    const bookURL = 'https://book.squareup.com/appointments/ysiif4g893rlw0/location/LP1D86FB8MBJ9/services/';
+
+    return bookURL + itemID;
+}
