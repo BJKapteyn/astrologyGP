7169789c562b93e611693515c0305fa8f36e4f6d
Neville Bamshoot <youcanteat@hotmail.com>
2024-11-09 21:43:14 -0500

    added store page

--- src/Pages/StorePage.jsx
+++ src/Pages/StorePage.jsx
@@ -1,22 +1,33 @@
-import { useEffect } from "react";
+import { useState } from "react";
+import { useGetAzureFunction } from "../CustomHooks/useGetAzureFunction.jsx";
+import { GetProductItems } from "../Enums/FunctionNames.js";
 import { SectionDivider } from "../Components/PageElements/SectionDivider/SectionDivider";
 import { StoreCategories } from '../Components/StorePage/StoreCategories/StoreCategories.jsx';
+import { LoadingIndicator } from "../Components/PageElements/LoadingIndicator/LoadingIndicator.jsx";
+import { buildAzureFunctionURL } from "../Functions/urlBuilders.js";
 
 export default function StorePage() {
+    const [itemData, setItemData] = useState(null);
+    const getProductItemsUrl = buildAzureFunctionURL(GetProductItems, process.env.REACT_APP_GET_PRODUCT_ITEMS)
+
+    const azureItemData = useGetAzureFunction(getProductItemsUrl);
 
     const storetitleSettings = {
         classname: '',
         headingText: 'STORE'
     }
 
-    useEffect(() => {
-        fetch()
-    })
+    if(azureItemData && itemData === null) {
+        setItemData(azureItemData);
+    }
 
+    if(itemData === null) {
+        return <LoadingIndicator></LoadingIndicator>
+    }
     return (
         <div className="store-page">
             <SectionDivider dividerData={storetitleSettings} />
-            <StoreCategories />
+            <StoreCategories squareStoreItems={itemData} />
         </div>
     );
 };
