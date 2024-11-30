4eb7544785b22e9ab0fb21a78c475f9405703639
Neville Bamshoot <youcanteat@hotmail.com>
2024-11-29 19:21:29 -0500

    fixed loading indicator to have a timeout

--- src/Components/PageElements/LoadingIndicator/LoadingIndicator.jsx
+++ src/Components/PageElements/LoadingIndicator/LoadingIndicator.jsx
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../../../App.css';
-import './LoadingIndicator.css'; 
+import './LoadingIndicator.css';
 
-export const LoadingIndicator = () => {
-  return (
-    <div id="loading-box">
-      <p id="loading-text">Loading Vibes...</p>
-    </div>
-  );
+export const LoadingIndicator = ({ message }) => {
+	const [isTimedOut, setIsTimedOut] = useState(false);
+	const [timedOutMessage, setTimedOutMessage] = useState(
+		'Sorry something went wrong, refresh or try again later'
+	);
+
+	useEffect(() => {
+		if (message) {
+			setTimedOutMessage(message);
+		}
+
+		const timeout = setTimeout(() => {
+			setIsTimedOut(true);
+		}, 7000);
+
+		return () => clearTimeout(timeout);
+	}, [message]);
+
+	if (isTimedOut) {
+		return (
+			<div id="loading-box">
+				<p className="loading-message">{timedOutMessage}</p>
+			</div>
+		);
+	}
+
+	return (
+		<div id="loading-box">
+			<p id="loading-text">Loading Vibes...</p>
+		</div>
+	);
 };
