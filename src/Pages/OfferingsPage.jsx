b62f3c228a2c3e3752a4dd2a5231e14e5699de12
Neville Bamshoot <youcanteat@hotmail.com>
2024-12-23 20:16:43 -0500

    more offeringspage cleanup

--- src/Pages/OfferingsPage.jsx
+++ src/Pages/OfferingsPage.jsx
@@ -6,15 +6,12 @@ import { ParentCategoryIds } from '../Enums/CategoryIds.js';
 import { FunctionNames } from '../Enums/FunctionNames.js';
 import { usePostAzureFunction } from '../CustomHooks/usePostAzureFunction.jsx';
 import { CategoryId } from '../Models/CategoryId.js';
-import { useLocation } from 'react-router-dom';
 
 // Page that shows all of the offered services
 export default function Offerings() {
     const endpointUrl = useRef(buildAzureFunctionURL(FunctionNames.GetCategoriesByCategoryId, process.env.REACT_APP_GET_CATEGORIES_BY_CATEGORY_ID));
     const categoryId = useRef(new CategoryId(ParentCategoryIds.Services));
-    const urlParams = useRef(useLocation());
     const [serviceItems, setServiceItemData] = useState(null);
-    console.log(urlParams);
     const azureItems = usePostAzureFunction(endpointUrl.current, categoryId.current, categoryId.current.Id);
 
     if(azureItems && !serviceItems) {
