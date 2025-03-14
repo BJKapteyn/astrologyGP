540aa76854293e9ca8af835cb79f7482caa82628
Neville Bamshoot <youcanteat@hotmail.com>
2025-03-14 13:59:37 -0400

    added istablet to empowerment link

--- src/Components/PageElements/SectionDivider/TitleDivider.jsx
+++ src/Components/PageElements/SectionDivider/TitleDivider.jsx
@@ -28,9 +28,11 @@ export const TitleDivider = () => {
                     <DividerGraphic isInBetween={true}></DividerGraphic>
                 </div>
             )}
-            <Link to={'http://localhost:3000/Offerings/Life%20Coaching-YBBDQEGFZ52IACA5CUXB2GEM/Empowerment%20Strategy%20Session-CBTEGWQKT4J4TGZJT4PEKCJR'}>
-                <h1 className="divider-text divider-futura divider-textglow link-colorfade">EMPOWERMENT STRATEGY</h1>
-            </Link>
+            {isTablet && (
+                <Link to={'http://localhost:3000/Offerings/Life%20Coaching-YBBDQEGFZ52IACA5CUXB2GEM/Empowerment%20Strategy%20Session-CBTEGWQKT4J4TGZJT4PEKCJR'}>
+                    <h1 className="divider-text divider-futura divider-textglow link-colorfade">EMPOWERMENT STRATEGY</h1>
+                </Link>
+            )}
             {isTablet && (
                 <div className="divider-titleGraphic">
                     <DividerGraphic isInBetween={true}></DividerGraphic>
