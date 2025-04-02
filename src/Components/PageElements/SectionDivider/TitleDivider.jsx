568c546dff8eef2b09077ac66982c4b64c7004f6
Neville Bamshoot <youcanteat@hotmail.com>
2025-04-01 21:31:18 -0400

    homepage divider hotfix

--- src/Components/PageElements/SectionDivider/TitleDivider.jsx
+++ src/Components/PageElements/SectionDivider/TitleDivider.jsx
@@ -12,7 +12,7 @@ export const TitleDivider = () => {
             <div className="divider-titleGraphic">
                 <DividerGraphic needsFlip={true}></DividerGraphic>
             </div>
-            <Link  to={'http://localhost:3000/Offerings/Astrology-SWLZ6MMXKJSRVKRNRVKXVQGJ'}>
+            <Link  to={'https://thevibecollective.co/Offerings/Astrology-SWLZ6MMXKJSRVKRNRVKXVQGJ'}>
                 <h1 className="divider-text divider-futura link-colorfade">ASTROLOGY</h1>
             </Link>
             {isTablet && (
@@ -20,7 +20,7 @@ export const TitleDivider = () => {
                     <DividerGraphic isInBetween={true}></DividerGraphic>
                 </div>
             )}
-            <Link to={'http://localhost:3000/Offerings/Tarot-BO3VI3MRP5O4QUSPWJPHB7WJ'}>
+            <Link to={'https://thevibecollective.co/Offerings/Tarot-BO3VI3MRP5O4QUSPWJPHB7WJ'}>
                 <h1 className="divider-text divider-futura divider-textglow link-colorfade">TAROT READINGS</h1>
             </Link>
             {isTablet && (
@@ -29,7 +29,7 @@ export const TitleDivider = () => {
                 </div>
             )}
             {isTablet && (
-                <Link to={'http://localhost:3000/Offerings/Life%20Coaching-YBBDQEGFZ52IACA5CUXB2GEM/Empowerment%20Strategy%20Session-CBTEGWQKT4J4TGZJT4PEKCJR'}>
+                <Link to={'https://thevibecollective.co/Offerings/Life%20Coaching-YBBDQEGFZ52IACA5CUXB2GEM/Empowerment%20Strategy%20Session-CBTEGWQKT4J4TGZJT4PEKCJR'}>
                     <h1 className="divider-text divider-futura divider-textglow link-colorfade">EMPOWERMENT STRATEGY</h1>
                 </Link>
             )}
@@ -38,7 +38,7 @@ export const TitleDivider = () => {
                     <DividerGraphic isInBetween={true}></DividerGraphic>
                 </div>
             )}
-            <Link to={'http://localhost:3000/Offerings/Mediumship-WBIUSSJV2DQ2TXNJVNWIT6A3'}>
+            <Link to={'https://thevibecollective.co/Offerings/Mediumship-WBIUSSJV2DQ2TXNJVNWIT6A3'}>
                 <h1 className="divider-text divider-futura link-colorfade">MEDIUMSHIP</h1>
             </Link>
             <div className="divider-titleGraphic">
@@ -47,20 +47,20 @@ export const TitleDivider = () => {
         </section>
     );
 }
-export const titleDividerData = [
-    {
-        id: 1,
-        classname: 'divider-text divider-futura',
-        headingText: 'TAROT READINGS'
-    },
-    {
-        id: 2,
-        classname: 'divider-text divider-futura divider-textglow',
-        headingText: 'ASTROLOGY'
-    },
-    {
-        id: 3,
-        classname: 'divider-text divider-futura',
-        headingText: 'MEDIUMSHIP'
-    },
-]
+// export const titleDividerData = [
+//     {
+//         id: 1,
+//         classname: 'divider-text divider-futura',
+//         headingText: 'TAROT READINGS'
+//     },
+//     {
+//         id: 2,
+//         classname: 'divider-text divider-futura divider-textglow',
+//         headingText: 'ASTROLOGY'
+//     },
+//     {
+//         id: 3,
+//         classname: 'divider-text divider-futura',
+//         headingText: 'MEDIUMSHIP'
+//     },
+// ]
