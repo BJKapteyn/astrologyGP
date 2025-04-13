6f5d17bdd1e9b22e9cee4b6588c2e8fb031409a7
Neville Bamshoot <youcanteat@hotmail.com>
2025-04-13 15:22:23 -0400

    Added big calendar to package and added it to event page

--- src/Pages/EventCalendarPage.jsx
+++ src/Pages/EventCalendarPage.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
 
 export default function EventCalendarPage() {
+    const localizer = momentLocalizer(moment); // Set the localizer by providing the moment Object to the correct localizer.
+    const events = [
+        {
+            title: 'Board meeting',
+            start: new Date(2025, 4, 14, 10, 0),
+            end: new Date(2025, 4, 14, 11, 0),
+        },
+        {
+            title: 'Team lunch',
+            start: new Date(2025, 4, 15, 12, 0),
+            end: new Date(2025, 4, 15, 13, 0),
+        },
+    ];
+    const calendar = Calendar({ localizer, events: events, startAccessor: 'start', endAccessor: 'end' });
+    
     return (
         <div>
             <h1>Event Calendar Page</h1>
+            {calendar}
         </div>
     );
 }
