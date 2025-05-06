5e2cef8b2832e122d763edea7b9bf9314acbbdb0
Neville Bamshoot <youcanteat@hotmail.com>
2025-05-06 13:45:20 -0400

    cleanup

--- src/Components/PageElements/SingleItem/SingleItem.tsx
+++ src/Components/PageElements/SingleItem/SingleItem.tsx
@@ -3,8 +3,8 @@ import { ItemData } from "Models/Interfaces/ItemData";
 import { Link } from "react-router-dom";
 import { useRandomImageUrl } from "CustomHooks/useRandomImageUrl";
 import { ActionButton } from "Components/PageElements/ActionButton/ActionButton";
-import './SingleItem.css';
 import { LoadingIndicator } from "../LoadingIndicator/LoadingIndicator";
+import './SingleItem.css';
 
 interface SingleItemProps {
     itemData: ItemData;
@@ -18,7 +18,6 @@ export const SingleItem: React.FC<SingleItemProps> = ({
         itemData,
         defaultBuyNowURL = '', 
         purchaseButtonText = 'BUY NOW', 
-        printVariationBuyButtons = false 
     }) => {
 
     const [purchaseLink, setPurchaseLink] = useState(itemData?.buyNowLink);
@@ -47,24 +46,21 @@ export const SingleItem: React.FC<SingleItemProps> = ({
             <div className="singleitem-information-container">
                 <div className="singleitem-information">
                     <p id="singleitem-name">{itemData.name.toUpperCase()}</p>
-                    {!!printVariationBuyButtons === false
-                        ? <Link target='_blank' to={purchaseLink}><ActionButton buttonSettings={purchaseButtonSettings}></ActionButton></Link>
-                        : itemData.variations.map(variation => {
-                            return (
-                                <div key={variation.id+variation.name} className="singleitem-variation-container">
-                                    <p className="singleitem-variation">{variation.name.toUpperCase()}</p>
-                                    <Link target='_blank' to={purchaseLink}><ActionButton buttonSettings={purchaseButtonSettings}></ActionButton></Link>
-                                </div>
-                            );
-                        })
-                    }
+                   {!hasVariation && <Link target='_blank' to={purchaseLink}><ActionButton buttonSettings={purchaseButtonSettings}></ActionButton></Link>}
+                                       {hasVariation && itemData.variations.map(variation => {
+                                           return (
+                                               <div key={variation.id+variation.name} className="singleitem-variation-container">
+                                                   <p className="singleitem-variation">{variation.name.toUpperCase()}</p>
+                                                    <Link target='_blank' to={purchaseLink}><ActionButton buttonSettings={purchaseButtonSettings}></ActionButton></Link>
+                                               </div>
+                                           )
+                                       })}
                     {itemData.description && (hasVariation ? <p id="singleitem-descriptiontitle">DESCRIPTION</p> : <p style={{borderTop: 'none'}} id="singleitem-descriptiontitle">DESCRIPTION</p>)}
                     {itemData.description && <p id="singleitem-description">{itemData.description}</p>}
                     <br></br>
                     <br></br>
                     <br></br>
                 </div>
-                {/* <ActionButton buttonSettings={backButtonSettings}></ActionButton> */}
             </div>
         </section>
 
