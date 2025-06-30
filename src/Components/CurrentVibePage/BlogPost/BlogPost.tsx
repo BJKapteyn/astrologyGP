44dc3b5692dcb6a4dd148432d6f89a2e6d6796bd
Neville Bamshoot <youcanteat@hotmail.com>
2025-06-30 14:48:10 -0400

    created blogpost component and css rulesets

---
+++ src/Components/CurrentVibePage/BlogPost/BlogPost.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+type Blog = {
+    id: string;
+    title: string;
+    author: string;
+    date: string;
+    content: string;
+};
+
+type BlogPostProps = {
+    blog: Blog;
+};
+
+const BlogPost: React.FC<BlogPostProps> = ({ blog }) => {
+    return (
+        <div
+            className="blogpost-container"
+            style={{ border: '1px solid #ccc', padding: '1rem', borderRadius: '8px' }}
+        >
+            <h2 className="blogpost-title">{blog.title}</h2>
+            <p className="blogpost-meta">
+            <strong className="blogpost-author">{blog.author}</strong> &middot; <em className="blogpost-date">{blog.date}</em>
+            </p>
+            <div className="blogpost-content">{blog.content}</div>
+        </div>
+    );
+};
+
+export default BlogPost;
