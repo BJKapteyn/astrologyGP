945a2e6801c4190eea4c969e076c071a9426a78a
Neville Bamshoot <youcanteat@hotmail.com>
2025-07-31 20:58:34 -0400

    merged createblogform

--- src/Components/EditPage/EditForm/CreateBlogForm/CreateBlogForm.tsx
+++ src/Components/EditPage/EditForm/CreateBlogForm/CreateBlogForm.tsx
@@ -9,28 +9,26 @@ interface CreateBlogFormProps {
 export const CreateBlogForm: React.FC<CreateBlogFormProps> = ({ blog = null }) => {
   const [blogData, setBlogData] = useState<Blog | null>(blog);
 
-    const handleSubmit = (formEvent: React.FormEvent<HTMLFormElement>) => {
-        formEvent.preventDefault();
-        // Handle form submission logic here
-        console.log("Form submitted:", blogData);
-    };
+  const handleSubmit = (formEvent: React.FormEvent<HTMLFormElement>) => {
+    formEvent.preventDefault();
+    // Handle form submission logic here
+    console.log("Form submitted:", blogData);
+  };
 
-    return (
-        <div className="edit-form" >
-            <h1>Create Blog Form</h1>
-            <form onSubmit={handleSubmit}>
-                {/* Form elements will go here */}
-                <label htmlFor="title">Title:</label>
-                <input type="text" id="title" name="title" defaultValue={blogData?.title || ''} />
-                <label htmlFor="content">Content:</label>
-                <textarea id="content" name="content" defaultValue={blogData?.content || ''} />
-                <label htmlFor="author">Author:</label>
-                <input type="text" id="author" name="author" defaultValue={blogData?.author || ''} />
-                <label htmlFor="publishDate">Publish Date</label>
-                <input type="date" id="publishDate" name="publishDate" defaultValue={blogData?.publishDate || ''} />
-                <button type="submit">Submit</button>
-            </form>
-            {/* Add form elements here */}
-        </div>
-    );
+  return (
+    <div className="edit-form" >
+      <h1>Create Blog Form</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="title">Title:</label>
+        <input type="text" id="title" name="title" defaultValue={blogData?.title || ''} />
+        <label htmlFor="content">Content:</label>
+        <textarea id="content" name="content" defaultValue={blogData?.content || ''} />
+        <label htmlFor="author">Author:</label>
+        <input type="text" id="author" name="author" defaultValue={blogData?.author || ''} />
+        <label htmlFor="publishDate">Publish Date</label>
+        <input type="date" id="publishDate" name="publishDate" defaultValue={blogData?.publishDate || ''} />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
