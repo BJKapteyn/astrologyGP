35c49d4d626afdbda636552a1166383cd4f3da71
Brad K <bjkapteyn@gmail.com>
2025-12-10 11:36:22 -0500

    swapped text for a variable and added meta data to currentvibe and offerings page

--- src/Pages/CurrentVibePage.jsx
+++ src/Pages/CurrentVibePage.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { BlogSection } from '../Components/CurrentVibePage/BlogSection/BlogSection.tsx';
 import { addPageHeaderInfo } from 'UtilityFunctions/pageUtility.js';
 
@@ -5,9 +6,12 @@ import { addPageHeaderInfo } from 'UtilityFunctions/pageUtility.js';
 function CurrentVibePage() {
     const keywords = 'Current Vibe, Vibe Collective Blog, Spiritual Blog, Astrology Articles, Wellness Tips, Mindfulness Practices, Holistic Living, Personal Growth, Spirituality Insights, Vibe Collective Updates';
     const description = 'Stay updated with the latest insights, articles, and news from The Vibe Collective. Explore our blog for tips on spirituality, wellness, and personal growth.';
+    const title = 'Current Vibe - The Vibe Collective';
 
-    addPageHeaderInfo(description, keywords, 'Current Vibe - The Vibe Collective');
-    
+    useEffect(() => {   
+        addPageHeaderInfo(description, keywords, title);
+    }, []);
+
     return (
         <BlogSection />
     );
