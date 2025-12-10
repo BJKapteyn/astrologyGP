f66149666c4c699de06aec40c39bed48722f13de
Brad K <bjkapteyn@gmail.com>
2025-12-10 11:31:46 -0500

    removed unused code, added privacy policy page

---
+++ src/Pages/PrivacyPolicyPage.tsx
@@ -0,0 +1,7 @@
+import { PrivacyPolicy } from "../Components/PrivacyPolicy/PrivacyPolicy";
+
+export const PrivacyPolicyPage = () => {
+    return <PrivacyPolicy />;
+};
+
+export default PrivacyPolicyPage;
