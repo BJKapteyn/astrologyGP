023e2f00bcd605729024680fdd0ea6a5691201a7
Brad K <bjkapteyn@gmail.com>
2026-01-27 11:31:52 -0500

    remove TikTok Pixel and Google Tag Manager scripts from index.html; add smooth scroll to SingleBlogPost componentjn

--- src/Components/CurrentVibePage/SingleBlogPost/SingleBlogPost.tsx
+++ src/Components/CurrentVibePage/SingleBlogPost/SingleBlogPost.tsx
@@ -4,14 +4,25 @@ import { BlogPost } from '../BlogPost/BlogPost';
 import { buildAzureFunctionURL } from 'UtilityFunctions/urlUtility';
 import { LoadingIndicator } from 'Components/PageElements/LoadingIndicator/LoadingIndicator';
 import { Blog } from 'Models/Types/types';
+import { useEffect, useRef } from 'react';
 
 export const SingleBlogPost: React.FC = () => {
     const url: string = buildAzureFunctionURL('GetBlogPostById', process.env.REACT_APP_GET_BLOG_POST_BY_ID);
     const {blogId, partKey} = useParams();
     const blogPost: Blog | null = usePostAzureFunction(url, { id: blogId, partitionKey: partKey }, blogId);
+    const top = useRef<HTMLDivElement>(null);
+    
+    useEffect(() => {
+        top.current?.scrollIntoView({ behavior: "smooth" });
+    }, []);
 
     if (!!blogPost) {
-        return <BlogPost blog={blogPost} />;
+        return (
+            <div style={{paddingBottom: '4em'}}>
+                <div ref={top}></div>
+                <BlogPost blog={blogPost} />
+            </div>
+        );
     } else {
         return <LoadingIndicator />;
     }
--- src/Components/HomePage/LatestVideo/LatestVideo.tsx
+++ src/Components/HomePage/LatestVideo/LatestVideo.tsx
@@ -64,7 +64,7 @@ export const LatestVideo: React.FC = () => {
                     src={`https://www.youtube.com/embed/${latestVideo.id}`}
                     referrerPolicy='strict-origin-when-cross-origin'
                     title={latestVideo.title}
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    // allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                     allowFullScreen
                 />
             </div>
